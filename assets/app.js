e92a9d11e58dab14b5c5493492780d2bdd058adf
Vasyl <uacyberdevgroup@gmail.com>
2020-12-08 17:15:17 +0200

    Commit changes in app.js

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,12 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+// importing Bootstrap sources
+import './styles/bootstrap/bootstrap.css';
+import './styles/bootstrap/bootstrap-grid.css';
+import './styles/bootstrap/bootstrap-reboot.css';
+import './styles/bootstrap/bootstrap-utilities.css';
+
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
