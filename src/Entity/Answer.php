6d3bfd78da433e2a8d17a010eff1edfad6e5feb0
Vasyl <uacyberdevgroup@gmail.com>
2020-12-08 14:58:39 +0200

    Initiated repository. Created entities: Question, Anwser. Created CRUD controller

---
+++ src/Entity/Answer.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AnswerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AnswerRepository::class)
+ */
+class Answer
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $anwserText;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Question::class, inversedBy="answers")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $question;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAnwserText(): ?string
+    {
+        return $this->anwserText;
+    }
+
+    public function setAnwserText(string $anwserText): self
+    {
+        $this->anwserText = $anwserText;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getQuestion(): ?Question
+    {
+        return $this->question;
+    }
+
+    public function setQuestion(?Question $question): self
+    {
+        $this->question = $question;
+
+        return $this;
+    }
+}
